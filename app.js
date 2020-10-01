4332ac5ff7fd15497768d1cbab04f1334f71a5b1
Jessica <jessicacotrina@gmail.com>
2020-10-01 17:30:07 -0400

    Improving our Model: Hooks

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+
 app.use(express.urlencoded({
-  extended: false
+  extended: true
 }))
+//
+// app.use(express.json())
 
 app.use('/wiki', wiki)
 
 const { db } = require('./models');
 
-//app.get("/", (req, res) => {
- // console.log('hello world!');
-//})
+app.get("/", (req, res) => {
+ res.redirect('/wiki')
+})
 
 db.authenticate().then(() => {
   console.log('Connected to the database!');
--- routes/wiki.js
+++ routes/wiki.js
@@ -2,18 +2,34 @@ const router = require('express').Router();
 //const client = require('../models');
 const addPage = require('../views/addPage');
 //const { wikiPage } = require('../views');
+const { Page } = require('../models')
 
 router.get('/', (req, res, next) => {
   res.send('go to get wiki')
 })
 
-router.post('/', (req, res, next) => {
-  res.send('go to post wiki')
+router.post('/', async (req, res, next) => {
+  
+  try {
+    const page = await Page.create({
+      title: req.body.title,
+      content: req.body.content
+    });
+  res.redirect('/');
+  } catch (error) {
+    console.error(error);
+  }
+  console.log("NAME"+req.body.name);
+  // res.send('go to post wiki')
 })
 
 router.get('/add', (req, res, next) => {
   //res.send('Hello world');
   res.send(addPage());
 })
+
+function generateSlug(title){
+  return title.replace(,'_');
+}
 
 module.exports = router;
